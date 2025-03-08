1c59807e2af570b8fd53d1550d12400cce78351d
Hofacker <matt.hofacker@gmail.com>
2025-03-08 13:51:00 -0500

    Create global.js

---
+++ src/scripts/global.js
@@ -0,0 +1,12 @@
+// src/scripts/global.js
+
+// Import all global scripts here
+import '../utils/globalSearch';
+import './mobile-fixes';
+
+// Add any additional global functionality here
+
+// Make sure all scripts run on page load
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('FemTechURLs global scripts initialized');
+});
